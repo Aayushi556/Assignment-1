e09ab14d629cb7c7fa58bd7ec6e744bad6e7a97a
Aayushi556 <kadiyanaaru17@gmail.com>
2025-08-22 10:00:41 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+num1= int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sum= num1+num2
+sub= num1-num2
+mul=num1*num2
+div= num1/num2
+print("the sum is",sum)
+print("the sub is",sub)
+print("the multiplication is",mul)
+print("division= ",div)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+name=input("enter the name:")
+l_name=input("enter the last name :")
+full_name = name +' '+l_name
+print(full_name)
+print("hey "+ full_name+ " welcome to university.")
